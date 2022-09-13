187ce6adf7878621df481e573c60d28aa45dafef
Iwona <pietrucha-iwona@wp.pl>
2022-09-13 14:11:50 +0200

    Flickr API complete

--- src/Map.js
+++ src/Map.js
@@ -10,24 +10,18 @@ import { GoogleMap, LoadScript, MarkerF, InfoWindowF } from "@react-google-maps/
 class MapContainer extends Component {
 
 state = {
-    animation: null
+    link: ''
     }
   
 /*  onSelect=(restaurant) => {
     this.setState({select: restaurant})
   }*/
-i/*coneSet = (restaurant) => {
-    this.setState({url:'http://maps.google.com/mapfiles/ms/icons/yellow.png' })
+/*if (selected) {
+  this.setState({select: restaurant})
 }*/
-setA = (restaurant) => {
-  this.setState({animation: 1})
-}
-setN = () => {
-  this.setState({animation: null})
-}
-  /*setSelected=()=> {
-    this.setState({selected: null})
-  }*/
+  set=(Url)=> {
+    this.setState({link: Url})
+  }
 
     render () {
   const mapStyles = {        
@@ -39,7 +33,7 @@ setN = () => {
   const defaultCenter = {
     lat: 50.049683, lng: 19.944544
   }
-  const {restaurants, onSelect, selected, setSelected, setA, animation, setN} = this.props
+  const {restaurants, onSelect, selected, setSelected, url, animation, setN} = this.props
 
   const image = {
     url: '../map-marker.png',
@@ -47,8 +41,27 @@ setN = () => {
   //const {selected} = this.state
 
   //const [ selected, setSelected ] = useState({});
-
-
+ let Url, adres, adresik
+  
+  if(selected!==null) {
+    adres = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c30015377c78bd8635055c5ea59e145e&lat=" +selected.geometry.location.lat+"&lon=" +selected.geometry.location.lng +"&per_page=5&page=1&format=json&nojsoncallback=1"
+    fetch(adres).then(data=> {
+  return data.json()
+}).then(jsonData => {
+  console.log(adres);
+ console.log(jsonData.photos);
+ //console.log('https://farm' + jsonData.photos.photo[1].farm + '.staticflickr.com/' + jsonData.photos.photo[1].server + '/' + jsonData.photos.photo[1].id + '_' + jsonData.photos.photo[1].secret + '.jpg');
+ Url = 'https://farm' + jsonData.photos.photo[0].farm + '.staticflickr.com/' + jsonData.photos.photo[0].server + '/' + jsonData.photos.photo[0].id + '_' + jsonData.photos.photo[0].secret + '.jpg';
+console.log(Url);
+return Url;
+//this.set();
+}).catch(error=> {
+  console.log(error);
+}) 
+      adresik = Url;
+      console.log(adresik);
+      //Url = "https://farm" + jsonData.photos.photo[1].farm + ".staticflickr.com/" + jsonData.photos.photo[1].server + "/" + jsonData.photos.photo[1].id + '_' + jsonData.photos.photo[1].secret + '.jpg'
+  }
 /*  const onSelect = item => {
     setSelected(item);
   }
@@ -71,8 +84,9 @@ setN = () => {
             <MarkerF key={restaurant.name} 
             position={restaurant.geometry.location}
             animation={selected!==null ? (selected===restaurant? '1' : '0') : '0'}
+            color="blue"
             //animation={animation}
-            //icon={selected===restaurant? selected.icon: "http://maps.google.com/mapfiles/ms/icons/blue.png"}
+            icon={selected!==null ? (selected===restaurant?  "http://maps.google.com/mapfiles/ms/icons/blue.png":''):''}
            /*icon={"http://maps.google.com/mapfiles/ms/icons/blue.png"}*/
             /*onClick ={()=>this.iconeSet(restaurant)}*/
             onClick={() => {onSelect(restaurant)}}
@@ -95,6 +109,7 @@ setN = () => {
             >
             <div>
               <p>{selected.name}</p>
+              <img alt="place" src={url}></img>
             </div>
             </InfoWindowF>
             ) :null
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,7 +9,7 @@ class Filter extends Component {
     query: '',
     showRestaurants: restaurants.results,
     selected: null,
-    animation: '1'
+    url: ''
 
   }
   
@@ -24,28 +24,72 @@ class Filter extends Component {
     this.setState({showRestaurants: showRestaurants});
   }
 
-  onSelect=(restaurant) => {
+  onSelect=(restaurant, Url) => {
+    this.dupa(restaurant);
+
     this.setState({selected: restaurant,
-                  animation:1});
+                  url: Url});
+                  //animation:1
 
   }
 
   setSelected=()=> {
     this.setState({selected: null,
-                  animation: '0'})
+                  url: ''})
+                  //animation: '0'
   }
 
-  setA = (restaurant) => {
+dupa = (selected) => {
+let adres, Url
+
+      adres = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c30015377c78bd8635055c5ea59e145e&lat=" +selected.geometry.location.lat+"&lon=" +selected.geometry.location.lng +"&per_page=5&page=5&format=json&nojsoncallback=1"
+    fetch(adres).then(data=> {
+  return data.json()
+}).then(jsonData => {
+  console.log(adres);
+ console.log(jsonData.photos);
+ 
+ //console.log('https://farm' + jsonData.photos.photo[1].farm + '.staticflickr.com/' + jsonData.photos.photo[1].server + '/' + jsonData.photos.photo[1].id + '_' + jsonData.photos.photo[1].secret + '.jpg');
+ Url = 'https://farm' + jsonData.photos.photo[0].farm + '.staticflickr.com/' + jsonData.photos.photo[0].server + '/' + jsonData.photos.photo[0].id + '_' + jsonData.photos.photo[0].secret + '.jpg';
+console.log(Url);
+this.setState({url: Url})
+//return Url;
+//this.set();
+}).catch(error=> {
+  console.log(error);
+  alert("Sorry, you can't see image now. There is problem with Flickr API. Refresh your browser or try agaian later")
+}) 
+      //adresik = Url;
+      //console.log(adresik);
+      //Url = "https://farm" + jsonData.photos.photo[1].farm + ".staticflickr.com/" + jsonData.photos.photo[1].server + "/" + jsonData.photos.photo[1].id + '_' + jsonData.photos.photo[1].secret + '.jpg'
+  }
+
+/*  setA = (restaurant) => {
   this.setState({animation: 1})
 }
 setN = (selected) => {
   this.setState({animation: null})
-}
+}*/
 	
   render() {
 
+const { query, selected} = this.state
+  /*  let adres
+      if(selected) {
+        adres = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c30015377c78bd8635055c5ea59e145e&lat=" +selected.geometry.location.lat+ "&" +selected.geometry.location.lng +"&per_page=5&page=1&format=json&nojsoncallback=1"
+    fetch(adres).then(data=> {
+  return data.json()
+}).then(jsonData => {
+ console.log(jsonData.photos);
+ console.log(adres);
+}).catch(error=> {
+  console.log(error);
+}) 
+     
+  }
+*/
 	//const { restaurants, onChange} = this.props
-  const { query} = this.state
+  //const { query, selected} = this.state
 
         //let showRestaurants
       
@@ -89,9 +133,11 @@ setN = (selected) => {
                       selected = {this.state.selected}
                       onSelect = {this.onSelect}
                       setSelected = {this.setSelected}
-                      setA= {this.setA}
-                      animation = {this.state.animation}
-                      setN= {this.setN}/>
+                      url = {this.state.url}
+                      //setA= {this.setA}
+                      //animation = {this.state.animation}
+                      //setN= {this.setN}
+                      />
                     
       </div>
 
